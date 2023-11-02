76c7a798ea8b70b1f72b511da3c381b5613ce7db
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-11-02 12:09:44 -0300

    added loop in carrossel

--- scripts/carrossel.js
+++ scripts/carrossel.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper('.swiper', {    
     spaceBetween: 10,
     slidesPerView: 3,
+    loop:true,
     pagination:{
         el: '.swiper-pagination',
         type : 'bullets',
